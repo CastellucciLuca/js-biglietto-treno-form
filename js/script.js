2431e4c21af7c379a6a20759d2e4a5aa51d747db
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-22 15:12:44 +0100

    first-commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const km = document.querySelector("input.kilometres");
+
+const age = document.querySelector("input.age");
+
+let button = document.queryCommandValue("button")
+
+//BUTTON CLICK
+button.addEventListener ("click", function (){
+	console.log (km)
+    console.log (age)
+})
